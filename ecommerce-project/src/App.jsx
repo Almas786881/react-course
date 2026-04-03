07fa7aebc00b6c05de1218268c99b1ce2db9706f
Almas <almas786@gmail.com>
2026-04-03 21:29:33 +0530

    generated html for homepage

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,7 +1,8 @@
 import { Routes,Route} from 'react-router';
 import {Homepage} from './Pages/homepage';
 import {Checkoutpage} from './Pages/Checkoutpage';
-import {Orderpage} from './Pages/orderpage';  
+import {Orderpage} from './Pages/orderpage'; 
+import {Trackingpage} from './Pages/trackingpage'; 
 
 import './App.css'
 
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<Homepage/>} />
       <Route path="checkout" element={<Checkoutpage/>} />
       <Route path="orders" element={<Orderpage/>} />
+      <Route path="tracking" element={<Trackingpage/>} />
     </Routes>
   )
 }
